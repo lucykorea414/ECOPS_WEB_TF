461d277552bf51b39f52c245512c8ed84026fd50
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-04-15 21:55:45 +0900

    update detail page

--- django_webserver/polls/views.py
+++ django_webserver/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -30,7 +30,18 @@ def index(request):
     # context is a dictionary mapping template variable names to Python objects.
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
