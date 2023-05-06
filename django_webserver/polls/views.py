8245d3bbb1a6c1f192e18463b08a8f780ceedc6d
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-05-07 00:34:43 +0900

    Update views.py
    changed to generic views

--- django_webserver/polls/views.py
+++ django_webserver/polls/views.py
@@ -2,51 +2,40 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
-# Create your views here.
-
-def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
 
 
-    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # output = ", ".join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-
-    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # template = loader.get_template("polls/index.html")
-    # context = {
-    #     "latest_question_list": latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-
-
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
     
-    # context is a dictionary mapping template variable names to Python objects.
-
-def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
+#     # context is a dictionary mapping template variable names to Python objects.
 
 
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
 
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
 
 
