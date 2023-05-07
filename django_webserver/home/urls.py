93eda3cdfce953782a48eaabcad25b0ee15088dc
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-05-07 10:57:39 +0900

    update home urls

--- django_webserver/home/urls.py
+++ django_webserver/home/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from . import views
 
-app_name = "polls"
+app_name = "home"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("", ),
+    path("login/", ),
+    path("logout/", ),
+    path("register/", ),
+    # path("", views.IndexView.as_view(), name="index"),
+    # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
