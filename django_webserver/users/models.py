b93b60cdfc1d33f8cfd6baa57dfebf3d7462a27a
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-05-13 18:21:33 +0900

    made user account model

--- django_webserver/users/models.py
+++ django_webserver/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+    id = models.CharField(db_column='ID', primary_key=True, max_length=20)  # Field name made lowercase.
+    password = models.CharField(max_length=45)
+    name = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'member'
