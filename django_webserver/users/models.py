b38fb847c605cf89433d19d5388f149a7f0adf6e
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-05-16 14:35:54 +0900

    fix profile model
    on_delete 속성 추가

--- django_webserver/users/models.py
+++ django_webserver/users/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL) # 한계정의 사용자를 가져옴
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # 한계정의 사용자를 가져옴
     mickname = models.CharField(max_length=40)
     profile_photo = models.ImageField(blank=True)
-    
+
