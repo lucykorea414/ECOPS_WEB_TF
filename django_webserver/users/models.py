61ddabe3e5e75a74b43e22de7c158dc53eff9f50
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-05-16 11:57:30 +0900

    add user models
    profile model

--- django_webserver/users/models.py
+++ django_webserver/users/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL) # 한계정의 사용자를 가져옴
+    mickname = models.CharField(max_length=40)
+    profile_photo = models.ImageField(blank=True)
+    
