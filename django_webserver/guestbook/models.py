f00fe92683bead1ce6d57cdc6deb97b5d97e2b36
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-05-19 13:39:21 +0900

    update guestbook models

--- django_webserver/guestbook/models.py
+++ django_webserver/guestbook/models.py
@@ -4,11 +4,10 @@ from django.conf import settings
 # Create your models here.
 
 class Guestbook(models.Model):
-    user = models(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # 한계정의 사용자를 가져옴
-    writer = models.CharField(max_length=20)
-    password = models.CharField(max_length=32)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL)
     contents = models.TextField()
     reg_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Guestbook({self.name}, {self.password}. {self.contents}, {self.reg_date}'
+        return f'Guestbook({self.contents}, {self.reg_date}'
