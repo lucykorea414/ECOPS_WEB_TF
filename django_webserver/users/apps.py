2be5b64e61652d3e70cb043cdca4f0eff9d762b0
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2023-05-28 10:50:27 +0900

    fix: 회원가입 시 프로필 자동 할당

--- django_webserver/users/apps.py
+++ django_webserver/users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
+
+    def ready(self):
+        import users.signals  # 해당 줄을 추가합니다
