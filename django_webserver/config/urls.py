666a419d74ef66a8a586b79ebefcfe6b09a60e69
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-06-13 03:21:56 +0900

    configure posts urls

--- django_webserver/config/urls.py
+++ django_webserver/config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('polls/', include("polls.urls")),
     path('', include("home.urls")),
     path('users/', include("users.urls")),
+    path('posts/', include("posts.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
