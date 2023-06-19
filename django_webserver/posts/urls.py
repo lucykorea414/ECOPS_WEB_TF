f913285fb2ca73555fcb247f2019ea6a84ec5c41
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-06-19 18:27:04 +0900

    fix 잔잔바리

--- django_webserver/posts/urls.py
+++ django_webserver/posts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path('<int:pk>', views.posts_view, name='posts_view'),
+    # path("", views.IndexView.as_view(), name="index"),
+    # path('<int:pk>', views.posts_view, name='posts_view'),
 ]
