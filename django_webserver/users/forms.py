f7adb46c43d5b936c1b3d45d126d444793779741
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-06-19 17:43:54 +0900

    add: password change 기능

--- django_webserver/users/forms.py
+++ django_webserver/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from .models import Comment, Profile
 
@@ -29,3 +29,11 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ['nickname', 'note', 'profile_photo'] 
         widgets = {'profile_photo': forms.FileInput(attrs={'accept': 'image/*'})}
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def clean_new_password2(self):
+        new_password1 = self.cleaned_data.get('new_password1')
+        new_password2 = self.cleaned_data.get('new_password2')
+        if new_password1 and new_password2 and new_password1 != new_password2:
+            raise forms.ValidationError("새 비밀번호가 일치하지 않습니다.")
+        return new_password2
